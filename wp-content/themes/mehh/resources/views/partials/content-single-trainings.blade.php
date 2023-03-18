34a9c8b11b704f0cb7e2c736c4e37ee88232f1f4
Madis Luik <madis.luik@eklips.se>
2023-03-18 20:16:33 +0200

    entry-content

--- wp-content/themes/mehh/resources/views/partials/content-single-trainings.blade.php
+++ wp-content/themes/mehh/resources/views/partials/content-single-trainings.blade.php
@@ -1,13 +1,15 @@
-<article @php(post_class())>
-  <header>
-    <h1 class="entry-title">
-      {!! $title !!}
-    </h1>
-    @include('partials/entry-meta-trainings-category')
-  </header>
+<div class="entry-content">
+  <article @php(post_class())>
+    <header>
+      <h1 class="entry-title">
+        {!! $title !!}
+      </h1>
+      @include('partials/entry-meta-trainings-category')
+    </header>
 
-  <div class="entry-content">
-    @php(the_content())
-    @include('partials/entry-meta-trainings-trainer')
-  </div>
-</article>
+    <div class="entry-content">
+      @php(the_content())
+      @include('partials/entry-meta-trainings-trainer')
+    </div>
+  </article>
+</div>
--- wp-content/themes/mehh/resources/views/partials/content-trainings.blade.php
+++ wp-content/themes/mehh/resources/views/partials/content-trainings.blade.php
@@ -1,18 +1,20 @@
-<article @php(post_class())>
-  <header>
-    <h2 class="entry-title">
-      <a href="{{ get_permalink() }}">
-        {!! $title !!}
-      </a>
-    </h2>
+<div class="entry-content">
+  <article @php(post_class())>
+    <header>
+      <h2 class="entry-title">
+        <a href="{{ get_permalink() }}">
+          {!! $title !!}
+        </a>
+      </h2>
 
-    <div class="d-flex gap-2 items-center mb-3">
-      @include('partials/entry-meta-trainings-date', ['left_aligned' => true])
-      @include('partials/entry-meta-trainings-category')
+      <div class="d-flex gap-2 items-center mb-3">
+        @include('partials/entry-meta-trainings-date', ['left_aligned' => true])
+        @include('partials/entry-meta-trainings-category')
+      </div>
+    </header>
+
+    <div class="entry-summary">
+      @php(the_excerpt())
     </div>
-  </header>
-
-  <div class="entry-summary">
-    @php(the_excerpt())
-  </div>
-</article>
+  </article>
+</div>
